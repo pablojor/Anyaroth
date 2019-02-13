007297fefd3f2c95ad9c9da80149a6760b4ae3c1
mariotab28 <38128815+mariotab28@users.noreply.github.com>
2019-02-13 10:52:28 +0100

    Update ArmControllerComponent.cpp

--- Anyaroth/Anyaroth/ArmControllerComponent.cpp
+++ Anyaroth/Anyaroth/ArmControllerComponent.cpp
@@ -1,6 +1,7 @@
 #include "ArmControllerComponent.h"
 #include "GameComponent.h"
 #include <cmath>
+#include <iostream>
 
 #define PI 3.14159265
 
@@ -25,35 +26,65 @@ void ArmControllerComponent::handleInput(const SDL_Event& event)
 		int x, y;
 		SDL_GetMouseState(&x, &y);
 
+
+		Vector2D direction = { (_transform->getPosition().getX() + _followC->getInitialOffset().getX() - (x)),
+			(_transform->getPosition().getY() + _followC->getInitialOffset().getY() - y) };
+
+		if (!_anim->isFlipped())
+		{
+			//direction = direction + Vector2D(42, 0);
+
+
+			if (x < _transform->getPosition().getX()) { //hago flip si el mouse está a la izquierda
+				_anim->flip();
+				_player->getComponent<AnimatedSpriteComponent>()->flip();
+				_transform->setAnchor(1 - _transform->getDefaultAnchor().getX(), _transform->getDefaultAnchor().getY());
+				//_transform->setPosition(0, 0);//_transform->getPosition().getX() - 40, _transform->getPosition().getY());
+				_followC->setOffset({ _followC->getInitialOffset().getX() - _followC->getInitialOffset().getX(), _followC->getInitialOffset().getY() });
+			}
+
+		}
+		else if (_anim->isFlipped())
+		{
+			//direction = direction + Vector2D(42, 0);
+
+			if (x > _transform->getPosition().getX() + _followC->getInitialOffset().getX()) {
+				_anim->unFlip();
+				_player->getComponent<AnimatedSpriteComponent>()->unFlip();
+				_transform->setAnchor(_transform->getDefaultAnchor().getX(), _transform->getDefaultAnchor().getY());
+				//_transform->setPosition(_transform->getPosition().getX() + 40, _transform->getPosition().getY());
+				_followC->setOffset({ _followC->getInitialOffset().getX(), _followC->getInitialOffset().getY() });
+			}
+		}
+
+		cout << (_transform->getPosition().getX()) << endl;
+
+		
+
+
+
+		direction.normalize();
+
 		//actualizo angulo del brazo
-		if (x < _transform->getPosition().getX() && !_anim->isFlipped()) //hago flip si el mouse está a la izquierda
-		{
-			_anim->flip();
-			_player->getComponent<AnimatedSpriteComponent>()->flip();
-			_transform->setAnchor(1-_transform->getDefaultAnchor().getX(), _transform->getDefaultAnchor().getY());
-			//_transform->setPosition(0, 0);//_transform->getPosition().getX() - 40, _transform->getPosition().getY());
-			_followC->setOffset({ _followC->getInitialOffset().getX() - _followC->getInitialOffset().getX(), _followC->getInitialOffset().getY() });
-		}
-		else if (x > _transform->getPosition().getX() && _anim->isFlipped())
-		{
-			_anim->unFlip();
-			_player->getComponent<AnimatedSpriteComponent>()->unFlip();
-			_transform->setAnchor(_transform->getDefaultAnchor().getX(), _transform->getDefaultAnchor().getY());
-			//_transform->setPosition(_transform->getPosition().getX() + 40, _transform->getPosition().getY());
-			_followC->setOffset({ _followC->getInitialOffset().getX(), _followC->getInitialOffset().getY() });
-		}
-
-
-
-		double rot = atan2((_transform->getPosition().getY() + _followC->getInitialOffset().getY() - y),
-			(_transform->getPosition().getX() - (x + _followC->getInitialOffset().getX()))) * 180.0 / PI;
+		double rot = atan2(direction.getY(), direction.getX()) * 180.0 / PI;
 
 		if (!_anim->isFlipped()) 
 		{
-			//rot -= 180;
+			rot -= 180 - 10;
+		}
+		else
+		{
+			rot -= 10;
 		}
 
-		_transform->setRotation(rot);
+
+		if (x > _transform->getPosition().getX() - 40 && x <= _transform->getPosition().getX() + 40
+			&& y > _transform->getPosition().getY() - 40 && y <= _transform->getPosition().getY() + 40) {
+		
+			;
+		}
+		else
+			_transform->setRotation(rot);
 	}
 
 
