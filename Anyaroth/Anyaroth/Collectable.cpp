cd5016f90f8d9171175a662ba081baf5ee65961d
DFaouaz <36241623+DFaouaz@users.noreply.github.com>
2019-02-17 20:31:44 +0100

    Coleccionables (Coin y AmmoPackage) (#12)
    * Creado Collectable y Storage
    
    * Clase de Coin y AmmoPackage añadidas
    
    * Clases añadidas a la solucion

--- Anyaroth/Anyaroth/Collectable.cpp
+++ Anyaroth/Anyaroth/Collectable.cpp
@@ -2,10 +2,15 @@
 #include "SpriteComponent.h"
 
 
-Collectable::Collectable(Texture* texture, Vector2D iniPos) : GameComponent()
+Collectable::Collectable(Texture* texture, Vector2D iniPos, int value) : _value(value), GameComponent()
 {
+	addComponent<Texture>(texture);
+
 	auto transform = addComponent<TransformComponent>();
+	transform->setPosition(iniPos.getX(), iniPos.getY());
+
 	addComponent<SpriteComponent>();
 
-	transform->setPosition(iniPos.getX(), iniPos.getY());
+	//auto body = addComponente<BodyComponent>();
+	//Modificacion del body (si es que necesita).
 }
