42a47037c8eb43b08123343c1337bca79f3012d9
MiriamLeis <mirileis@ucm.es>
2019-02-19 16:33:30 +0100

    Hecho sistema de dinero

---
+++ Anyaroth/Anyaroth/Money.h
@@ -0,0 +1,18 @@
+#pragma once
+
+class Money
+{
+	private:
+		int _bank = 0;
+		int _wallet = 0;
+
+	public:
+		Money() {}
+		~Money() {}
+
+		void restartWallet() { _wallet = 0; }
+		void storeWallet() { _bank += _wallet; }
+		void store(int s) { _wallet += s; }
+
+		bool spend(int n);
+};
