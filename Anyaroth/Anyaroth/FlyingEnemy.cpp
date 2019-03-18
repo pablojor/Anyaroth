e951d8bb633c7125f86472b4e84fa99f20f6bfd2
apoyatos <37510165+apoyatos@users.noreply.github.com>
2019-03-19 00:54:23 +0100

    Movimiento sinusoidal hecho falta seguimiento del jugador

--- Anyaroth/Anyaroth/FlyingEnemy.cpp
+++ Anyaroth/Anyaroth/FlyingEnemy.cpp
@@ -4,10 +4,16 @@
 
 FlyingEnemy::FlyingEnemy(Player* player, Game* g, PlayState* play, Texture* texture, Vector2D posIni, string tag) : Enemy( player,  g,  play,  texture, posIni, tag)
 {
-	_body->filterCollisions(ENEMIES, FLOOR);
+	_anim->addAnim(AnimatedSpriteComponent::EnemyIdle, 13, true);
+	_anim->addAnim(AnimatedSpriteComponent::EnemyDie, 18, false);
+
+	_anim->playAnim(AnimatedSpriteComponent::EnemyIdle);
+
+	_body->filterCollisions(ENEMIES, NULL);
 	_body->getBody()->SetGravityScale(0);
 
-	oY= _body->getBody()->GetTransform().p.y ;
+	_originalPos= Vector2D(_body->getBody()->GetPosition().x * M_TO_PIXEL, _body->getBody()->GetPosition().y * M_TO_PIXEL);
+	_playerTransform = _player->getComponent<TransformComponent>();
 }
 
 
@@ -17,9 +23,13 @@ FlyingEnemy::~FlyingEnemy()
 
 void FlyingEnemy::update(double time)
 {
-	double x = _body->getBody()->GetPosition().x +0.05;
-	double y = 5 * sin(1 * x - 0.05 * time)+ oY;
-	_body->getBody()->SetTransform(b2Vec2(x, y),0);
+	Enemy::update(time);
+	_velocity += 0.5;
+	_playerPos=_playerTransform->getPosition();
+	double angle = atan2(_transform->getPosition().getY() - _playerPos.getY(), _transform->getPosition().getX() - _playerPos.getX());
 
-	cout << x << " " << y<<endl;
+	double x = _originalPos.getX() + _velocity;
+	double y = _originalPos.getY() + _amplitude * sin(_k * x - _angularFrequency * time / 1000);
+
+	_body->getBody()->SetTransform(b2Vec2(x/M_TO_PIXEL, y/M_TO_PIXEL), 0);
 }
--- Anyaroth/Anyaroth/FlyingEnemy.h
+++ Anyaroth/Anyaroth/FlyingEnemy.h
@@ -4,7 +4,9 @@
 class FlyingEnemy :	public Enemy
 {
 private: 
-	double oY;
+	double _amplitude = 25, _angularFrequency = 0.05, _velocity = 0.5, _k = _angularFrequency / _velocity;
+	Vector2D _playerPos, _originalPos;
+	TransformComponent* _playerTransform;
 public:
 	FlyingEnemy(Player* player, Game* g, PlayState* play, Texture* texture, Vector2D posIni, string tag);
 	virtual ~FlyingEnemy();
