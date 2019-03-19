dd66ec6d1e027cbe90e81f4c7da39fad5d1cabbe
DFaouaz <dfaouaz@ucm.es>
2019-03-19 11:04:59 +0100

    Todo funcional con cambios de resolucion

--- Anyaroth/Anyaroth/FrameComponent.cpp
+++ Anyaroth/Anyaroth/FrameComponent.cpp
@@ -17,10 +17,11 @@ void FrameComponent::render(Camera* c) const
 	destRect.x = _transform->getPosition().getX() - _transform->getAnchor().getX() * destRect.w - c->getCameraPosition().getX();
 	destRect.y = _transform->getPosition().getY() - _transform->getAnchor().getY() * destRect.h - c->getCameraPosition().getY();
 
-	SDL_Point anchor = { _transform->getAnchor().getX() * destRect.w, _transform->getAnchor().getY() * destRect.h };
-
 	SDL_Rect winRect = { destRect.x * GAME_RESOLUTION_X / CAMERA_RESOLUTION_X ,destRect.y * GAME_RESOLUTION_Y / CAMERA_RESOLUTION_Y ,
 		destRect.w * GAME_RESOLUTION_X / CAMERA_RESOLUTION_X, destRect.h * GAME_RESOLUTION_Y / CAMERA_RESOLUTION_Y };
+
+	SDL_Point anchor = { _transform->getAnchor().getX() * winRect.w, _transform->getAnchor().getY() * winRect.h };
+
 
 	_texture->renderFrame(winRect, _fil, _col, _transform->getRotation(), anchor, (_flip ? SDL_FLIP_HORIZONTAL : SDL_FLIP_NONE));
 }
--- Anyaroth/Anyaroth/SpriteComponent.cpp
+++ Anyaroth/Anyaroth/SpriteComponent.cpp
@@ -17,10 +17,10 @@ void SpriteComponent::render(Camera* c) const
 	destRect.x = _transform->getPosition().getX() - _transform->getAnchor().getX() * destRect.w - c->getCameraPosition().getX();
 	destRect.y = _transform->getPosition().getY() - _transform->getAnchor().getY() * destRect.h - c->getCameraPosition().getY();
 
-	SDL_Point anchor = { _transform->getAnchor().getX() * destRect.w, _transform->getAnchor().getY() * destRect.h };
-
 	SDL_Rect winRect = {	destRect.x * GAME_RESOLUTION_X / CAMERA_RESOLUTION_X ,destRect.y * GAME_RESOLUTION_Y / CAMERA_RESOLUTION_Y ,
 							destRect.w * GAME_RESOLUTION_X / CAMERA_RESOLUTION_X, destRect.h * GAME_RESOLUTION_Y / CAMERA_RESOLUTION_Y };
+
+	SDL_Point anchor = { _transform->getAnchor().getX() * winRect.w, _transform->getAnchor().getY() * winRect.h };
 
 	_texture->render(winRect, _transform->getRotation(), anchor,(_flip ? SDL_FLIP_HORIZONTAL : SDL_FLIP_NONE));
 }
