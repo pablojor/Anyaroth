b7a23c294b0bb5fcf6beb1d06ea24ecc5db36d52
apoyatos <37510165+apoyatos@users.noreply.github.com>
2019-03-19 12:07:29 +0100

    Pruebas ondas

--- Anyaroth/Anyaroth/FlyingEnemy.cpp
+++ Anyaroth/Anyaroth/FlyingEnemy.cpp
@@ -9,7 +9,7 @@ FlyingEnemy::FlyingEnemy(Player* player, Game* g, PlayState* play, Texture* text
 
 	_anim->playAnim(AnimatedSpriteComponent::EnemyIdle);
 
-	_body->filterCollisions(ENEMIES, NULL);
+	_body->filterCollisions(ENEMIES, PLATFORMS, PLAYER);
 	_body->getBody()->SetGravityScale(0);
 
 	_originalPos= Vector2D(_body->getBody()->GetPosition().x * M_TO_PIXEL, _body->getBody()->GetPosition().y * M_TO_PIXEL);
@@ -24,12 +24,36 @@ FlyingEnemy::~FlyingEnemy()
 void FlyingEnemy::update(double time)
 {
 	Enemy::update(time);
-	_velocity += 0.5;
+	acumulatedVel += _velocity.getX();
 	_playerPos=_playerTransform->getPosition();
-	double angle = atan2(_transform->getPosition().getY() - _playerPos.getY(), _transform->getPosition().getX() - _playerPos.getX());
+	double angle = atan2(_playerPos.getY() - _transform->getPosition().getY(), _playerPos.getX() - _transform->getPosition().getX());
+	Vector2D dir = Vector2D(cos(angle), sin(angle));
+	Vector2D intDir = Vector2D((dir.getX() > 0) ? 1 : -1 , (dir.getY() > 0) ? 1 : -1);
 
-	double x = _originalPos.getX() + _velocity;
-	double y = _originalPos.getY() + _amplitude * sin(_k * x - _angularFrequency * time / 1000);
+	//Ondas Sinusoidales vertical y horizontal
+	double x = _body->getBody()->GetPosition().x * M_TO_PIXEL + _velocity.getX() * intDir.getX();
+	double y = _originalPos.getY() +_amplitude * sin(_k * x - _angularFrequency * time / 1000);
 
-	_body->getBody()->SetTransform(b2Vec2(x/M_TO_PIXEL, y/M_TO_PIXEL), 0);
+	double y1 = _body->getBody()->GetPosition().y * M_TO_PIXEL + _velocity.getY() * intDir.getY();
+	double x1 = _originalPos.getX() + _amplitude * cos(_k * y1 + _angularFrequency * time / 1000);
+
+	double x2 = _body->getBody()->GetPosition().x * M_TO_PIXEL + _velocity.getX() * intDir.getX();
+	double y2 = (_body->getBody()->GetPosition().y * M_TO_PIXEL + _velocity.getY() * intDir.getY()) + (_amplitude * sin(_angularFrequency * time / 1000));
+
+	double y3 = _body->getBody()->GetPosition().y * M_TO_PIXEL + _velocity.getY() * intDir.getY();
+	double x3 = _originalPos.getX()+ (acumulatedVel* intDir.getX()) + _amplitude * cos(_k * y1 + _angularFrequency * time / 1000);
+
+	_body->getBody()->SetTransform(b2Vec2(x2 / M_TO_PIXEL, y2 / M_TO_PIXEL), 0);
+
+	//Seguimiento del jugador
+	//_body->getBody()->SetLinearVelocity(b2Vec2(_velocity * cos(angle), _velocity * sin(angle)));
+
+
+
+
+}
+
+void FlyingEnemy::beginCollision(GameComponent * other, b2Contact * contact)
+{
+
 }
--- Anyaroth/Anyaroth/FlyingEnemy.h
+++ Anyaroth/Anyaroth/FlyingEnemy.h
@@ -4,12 +4,13 @@
 class FlyingEnemy :	public Enemy
 {
 private: 
-	double _amplitude = 25, _angularFrequency = 0.05, _velocity = 0.5, _k = _angularFrequency / _velocity;
-	Vector2D _playerPos, _originalPos;
+	Vector2D _playerPos, _originalPos, _velocity = Vector2D(0.5,0.5);
+	double _amplitude = 50, _angularFrequency = 0.5, acumulatedVel=0, _k = _angularFrequency / _velocity.distance(Vector2D());
 	TransformComponent* _playerTransform;
 public:
 	FlyingEnemy(Player* player, Game* g, PlayState* play, Texture* texture, Vector2D posIni, string tag);
 	virtual ~FlyingEnemy();
 	void update(double time);
+	void beginCollision(GameComponent * other, b2Contact* contact);
 };
 
