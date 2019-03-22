f0d10350abb1518d1a3ed051a11470e3da8ff5ee
MiriamLeis <mirileis@ucm.es>
2019-03-22 23:34:23 +0100

    Puedo ver la barra del boss

--- Anyaroth/Anyaroth/BossPanel.cpp
+++ Anyaroth/Anyaroth/BossPanel.cpp
@@ -7,8 +7,8 @@ BossPanel::BossPanel(Game * game) : PanelUI(game)
 	_lifeBar->setInUse(false);
 	_bossName = new TextUI(game, "BossName", game->getFont("ARIAL12"), 12, 0, 0, { 255, 255, 255, 255 });
 
-	_lifeBar->setPosition(GAME_RESOLUTION_X / 2 - _lifeBar->getW() / 2, GAME_RESOLUTION_Y - 80);
-	_bossName->setPosition(GAME_RESOLUTION_X / 2 - _bossName->getW() / 2, _lifeBar->getY() - _bossName->getH() - 1);
+	_lifeBar->setPosition(CAMERA_RESOLUTION_X / 2 - _lifeBar->getW() / 2, CAMERA_RESOLUTION_Y - 80);
+	_bossName->setPosition(CAMERA_RESOLUTION_X / 2 - _bossName->getW() / 2, _lifeBar->getY() - _bossName->getH() - 1);
 
 	addChild(_lifeBar);
 	addChild(_bossName);
