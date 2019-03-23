5ffcc575950535df8db02e7e16fdbd88a344580d
pablojor <pablojor@ucm.es>
2019-03-23 11:57:30 +0100

    Estructura inicial

--- Anyaroth/Anyaroth/Boss1.cpp
+++ Anyaroth/Anyaroth/Boss1.cpp
@@ -16,4 +16,50 @@ Boss1::Boss1(Player* player, Game* g, PlayState* play, Texture* texture, Vector2
 	_anim->addAnim(AnimatedSpriteComponent::EnemyAttack, 11, false);
 	_anim->playAnim(AnimatedSpriteComponent::EnemyIdle);
 	_body->addCricleShape(b2Vec2(0, _body->getH() + _body->getH() / 20), _body->getW() - _body->getW() / 20, ENEMIES, FLOOR | PLAYER_BULLETS | MELEE);
+
+	_prevPos = Vector2D(_body->getBody()->GetPosition().x * M_TO_PIXEL, _body->getBody()->GetPosition().y * M_TO_PIXEL);
+}
+
+void Boss1::update(double time)
+{
+	if (!_dead)
+	{
+		if (fase1)
+			Fase1(time);
+		else if (fase2)
+			Fase2(time);
+		else
+			Fase3(time);
+	}
+}
+void Boss1::movement(double time)
+{
+
+	_bodyPos = Vector2D(_body->getBody()->GetPosition().x * M_TO_PIXEL, _body->getBody()->GetPosition().y * M_TO_PIXEL);
+	//Onda Sinusoidal vertical 
+	double x = _bodyPos.getX() + _velocity.getX() *_dir.getX();
+
+	double prevY = _prevPos.getY() + _velocity.getY() *_dir.getY();
+	_prevPos = Vector2D(x, prevY);
+
+	double y = prevY + _amplitude * sin(_k * x - _angularFrequency * time / 1000);
+
+	_body->getBody()->SetTransform(b2Vec2(x / M_TO_PIXEL, y / M_TO_PIXEL), 0);
+}
+void Boss1::Fase1(double time)
+{
+
+}
+void Boss1::Fase2(double time)
+{
+
+}
+void Boss1::Fase3(double time)
+{
+
+}
+
+void Boss1::beetwenFases(double time)
+{
+
 }
--- Anyaroth/Anyaroth/Boss1.h
+++ Anyaroth/Anyaroth/Boss1.h
@@ -2,8 +2,21 @@
 #include "DistanceEnemy.h"
 class Boss1 : public DistanceEnemy
 {
-public:
-	Boss1(Player* player, Game* g, PlayState* play, Texture* texture, Vector2D posIni, string tag, BulletPool* pool);
-	virtual ~Boss1() {};
+	private:
+
+		bool fase1 = true, fase2 = false;
+		Vector2D _bodyPos, _prevPos, _velocity = Vector2D(0.5, 0.5), _dir = Vector2D(1,0);
+		double _amplitude = 25, _damage = 50, _angularFrequency = 0.05, _k = _angularFrequency / _velocity.distance(Vector2D());
+
+	public:
+		Boss1(Player* player, Game* g, PlayState* play, Texture* texture, Vector2D posIni, string tag, BulletPool* pool);
+		virtual ~Boss1() {};
+		virtual void update(double time);
+		void movement(double time);
+
+		void Fase1(double time);
+		void Fase2(double time);
+		void Fase3(double time);
+		void beetwenFases(double time);
 };
 
