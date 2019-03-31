0ba8ae6fa931c09cd550ac734f84607ac635e19e
pablojor <pablojor@ucm.es>
2019-03-31 12:59:32 +0200

    clase laser

--- Anyaroth/Anyaroth/Laser.cpp
+++ Anyaroth/Anyaroth/Laser.cpp
@@ -1,7 +1,7 @@
 #include "Laser.h"
 #include "Game.h"
 
-Laser::Laser(Game* g, Vector2D pos, Texture* texture, double w, double h, double damage) : GameObject(g, "Laser"), _damage(damage), _w(w), _h(h), _pos(pos)
+Laser::Laser(Game* g, Vector2D pos, Texture* texture, Player* player, double w, double h, double damage) : GameObject(g, "Laser"), _damage(damage), _w(w), _h(h), _pos(pos), _player(player)
 {
 	addComponent<Texture>(texture);
 
@@ -10,7 +10,22 @@ Laser::Laser(Game* g, Vector2D pos, Texture* texture, double w, double h, double
 
 	_anim = addComponent<SpriteComponent>();
 }
-
+void Laser::update(const double& deltaTime)
+{
+	if (isActive())
+	{
+		if (colliding)
+		{
+			if (startedDamaging >= timeToDmg)
+			{
+				_player->subLife(_damage);
+				startedDamaging = 0;
+			}
+			else
+				startedDamaging += deltaTime;
+		}
+	}
+}
 
 void Laser::Shoot(int angle)
 {
@@ -19,10 +34,36 @@ void Laser::Shoot(int angle)
 	_transform->setRotation(angle);
 	if (_body == nullptr)
 	{
-		addComponent<BodyComponent>();
+		_body = addComponent<BodyComponent>();
 
 		_body->filterCollisions(LASER, PLAYER);
 		_body->getBody()->SetType(b2_kinematicBody);
 		_body->getBody()->GetFixtureList()->SetSensor(true);
+	}
+}
+
+void Laser::Stop()
+{
+	if (_body != nullptr)
+	{
+		_body->deleteBody();
+		_body = nullptr;
+	}
+}
+
+void Laser::beginCollision(GameObject* other, b2Contact* contact)
+{
+	if (other->getTag() == "Player")
+	{
+		colliding = true;
+	}
+}
+
+void Laser::endCollision(GameObject* other, b2Contact* contact)
+{
+	if (other->getTag() == "Player")
+	{
+		colliding = false;
+		startedDamaging = 301;
 	}
 }
--- Anyaroth/Anyaroth/Laser.h
+++ Anyaroth/Anyaroth/Laser.h
@@ -3,6 +3,8 @@
 #include "BodyComponent.h"
 #include "AnimatedSpriteComponent.h"
 
+class Player;
+
 class Laser : public GameObject
 {
 protected:
@@ -10,18 +12,23 @@ protected:
 	TransformComponent* _transform;
 	BodyComponent* _body = nullptr;
 	Vector2D _pos;
+	Player* _player;
 	double _w, _h;
 	int _damage, _angle;
+	//Sistema de daño
+	double startedDamaging = 301, timeToDmg = 300;
+	bool colliding = false;
 
 public:
-	Laser(Game* g, Vector2D pos, Texture* texture,double w, double h,double damage = 5);
+	Laser(Game* g, Vector2D pos, Texture* texture, Player* player, double w, double h,double damage = 5);
 	virtual ~Laser() {}
 
-	virtual void update(const double& deltaTime) {}
+	virtual void update(const double& deltaTime);
 
 	void Shoot(int angle);
-
-	int const getDamage() { return _damage; }
+	void Stop();
+	virtual void beginCollision(GameObject* other, b2Contact* contact);
+	virtual void endCollision(GameObject* other, b2Contact* contact);
 
 	inline bool isActive() const { return (_body != nullptr && _body->getBody() != nullptr); }
 };
