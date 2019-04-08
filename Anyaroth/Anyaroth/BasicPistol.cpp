378e39c86a7715c6fd6461179b155bdbf3fd955a
argarc03 <argarc03@ucm.es>
2019-04-08 11:52:49 +0200

    añadido iconos de armas en el hud

--- Anyaroth/Anyaroth/BasicPistol.cpp
+++ Anyaroth/Anyaroth/BasicPistol.cpp
@@ -1,6 +1,6 @@
 #include "BasicPistol.h"
 
-BasicPistol::BasicPistol(Game* game) : Gun(game->getTexture("ArmPistol"), game->getTexture("PistolBullet"), 100, 10, 1000, 12, 60, 270, &_effect)
+BasicPistol::BasicPistol(Game* game) : Gun(game->getTexture("ArmPistol"), game->getTexture("PistolBullet"), 100, 10, 1000, 12, 60, 270, &_effect, false, Default, game->getTexture("PistolIcon"))
 {
 	_offset = { 24, -1 };
 }
--- Anyaroth/Anyaroth/GravityBombCannon.cpp
+++ Anyaroth/Anyaroth/GravityBombCannon.cpp
@@ -2,7 +2,7 @@
 
 
 
-GravityBombCannon::GravityBombCannon(Game* game) : Gun(game->getTexture("ArmShotgun"), game->getTexture("Coin"), 45, 5, 150, 3, 30, 400,  &_effect, false)
+GravityBombCannon::GravityBombCannon(Game* game) : Gun(game->getTexture("ArmShotgun"), game->getTexture("Coin"), 45, 5, 150, 3, 30, 400,  &_effect, false, Default, game->getTexture("bHCannonIcon"))
 {
 	_offset = { 24, -1 };
 }
