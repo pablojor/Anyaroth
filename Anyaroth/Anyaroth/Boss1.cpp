4662496b3a0ddd0a20a292bc73598ad7f672892f
xRyusey <xRyusey@users.noreply.github.com>
2019-04-19 17:36:55 +0200

    Arreglos mapa Boss1

--- Anyaroth/Anyaroth/Boss1.cpp
+++ Anyaroth/Anyaroth/Boss1.cpp
@@ -26,7 +26,6 @@ Boss1::Boss1(Game* g, Player* player, Vector2D pos, BulletPool* pool) : Boss(g, 
 	_orbGun->setBulletAnimType(BulletAnimType::SpentaOrb);
 	_orbGun->setBulletTexture(g->getTexture("SpentaOrb"));
 	_orbGun->setDamage(20);
-	
 
 	_attackRangeX = 120; //No se puede poner mas pequeño que la velocidad
 	_attackTime = 1300; //La animacion tarda unos 450
@@ -51,7 +50,7 @@ Boss1::Boss1(Game* g, Player* player, Vector2D pos, BulletPool* pool) : Boss(g, 
 	_body->filterCollisions(ENEMIES, FLOOR | PLAYER_BULLETS | MELEE | MISIL);
 	_body->getBody()->SetGravityScale(0);
 
-	_originalPos = pos;
+	_originalPos = Vector2D(pos.getX() + (_anim->getTexture()->getW() / 2) / _anim->getTexture()->getNumCols(), pos.getY() + (_anim->getTexture()->getW() / 2) / _anim->getTexture()->getNumCols());
 	_melee = new Axe(g, { 100, 0 }, PLAYER, 15, 50, 50, 270);
 	addChild(_melee);
 
@@ -83,6 +82,7 @@ void Boss1::update(const double& deltaTime)
 	{
 		_actualState = Moving;
 	}
+
 	if (_anim->animationFinished() && _anim->getCurrentAnim() == AnimatedSpriteComponent::SpentaEndBomb)
 	{
 		_actualState = Moving;
@@ -94,16 +94,17 @@ void Boss1::update(const double& deltaTime)
 			_anim->playAnim(AnimatedSpriteComponent::SpentaDie);
 		}
 	}
+
 	if (_anim->animationFinished() && _anim->getCurrentAnim() == AnimatedSpriteComponent::SpentaEndShield)
 	{
 		_actualState = Moving;
 	}
+
 	if (_anim->animationFinished() && _anim->getCurrentAnim() == AnimatedSpriteComponent::SpentaEndBomb)
 	{
 		_actualState = Moving;
 	}
 }
-
 
 void Boss1::movement(const double& deltaTime)
 {
@@ -111,14 +112,14 @@ void Boss1::movement(const double& deltaTime)
 	{
 		_anim->playAnim(AnimatedSpriteComponent::SpentaIdle);
 
-		if (((_bodyPos.getX() + _anim->getTexture()->getW() / 2 / _anim->getTexture()->getNumCols() > _originalPos.getX() + _amplitude.getX()) && _dir.getX() == 1) || ((_bodyPos.getX() + _anim->getTexture()->getW() / 2 / _anim->getTexture()->getNumCols() < _originalPos.getX() - _amplitude.getX()) && _dir.getX() == -1))
+		if (((_bodyPos.getX() + (_anim->getTexture()->getW() / 2) / _anim->getTexture()->getNumCols() > _originalPos.getX() + _amplitude.getX()) && _dir.getX() == 1) || ((_bodyPos.getX() + (_anim->getTexture()->getW() / 2) / _anim->getTexture()->getNumCols() < _originalPos.getX() - _amplitude.getX()) && _dir.getX() == -1))
 			_dir = Vector2D(-_dir.getX(), _dir.getY());
 
-		_playerPos = Vector2D(_playerBody->getBody()->GetPosition().x * M_TO_PIXEL, _playerBody->getBody()->GetPosition().y * M_TO_PIXEL);
 		_bodyPos = Vector2D(_body->getBody()->GetPosition().x * M_TO_PIXEL, _body->getBody()->GetPosition().y * M_TO_PIXEL);
+		_playerPos = Vector2D(_playerBody->getBody()->GetPosition().x * M_TO_PIXEL, _playerBody->getBody()->GetPosition().y * M_TO_PIXEL);
+
 		//Onda Sinusoidal vertical 
 		double x = _bodyPos.getX() + _velocity.getX() *_dir.getX();
-
 		double y = _originalPos.getY() + _amplitude.getY() * sin(_k * x - _angularFrequency * deltaTime / 1000);
 
 		_body->getBody()->SetTransform(b2Vec2(x / M_TO_PIXEL, y / M_TO_PIXEL), 0);
@@ -135,12 +136,14 @@ void Boss1::bomberAttack(const double& deltaTime, int t1, int t2)
 
 	_timeOnBomberAttack += deltaTime;
 	_armVision = false;
+
 	if (_timeOnBomberAttack >= _bomberAttackTime)
 	{
 		_timeOnBomberAttack = 0;
 		_timeBeetwenBombs = 0;
 		_armVision = true;
 		_doSomething = random(800, 1200);
+
 		if (_actualFase != BetweenFase)
 		{
 			_anim->playAnim(AnimatedSpriteComponent::SpentaEndBomb);
@@ -153,7 +156,6 @@ void Boss1::bomberAttack(const double& deltaTime, int t1, int t2)
 			throwBomb();
 			_timeBeetwenBombs += random(t1, t2);
 		}
-
 	}
 }
 
@@ -162,6 +164,7 @@ void Boss1::meleeAttack()
 	_bodyPos = Vector2D(_body->getBody()->GetPosition().x * M_TO_PIXEL, _body->getBody()->GetPosition().y * M_TO_PIXEL);
 	int dir = (_bodyPos.getX() >= _playerPos.getX()) ? -1 : 1;
 	_melee->meleeAttack(_bodyPos.getX(), _bodyPos.getY(), dir);
+
 	if (dir == -1)
 		_anim->playAnim(AnimatedSpriteComponent::SpentaSwordLeft);
 	else
@@ -177,14 +180,13 @@ void Boss1::checkMelee(const double& deltaTime)
 		if (_timeOnMelee > _timeMelee)
 		{
 			_melee->endMelee();
-			//Provisional
 			if (_actualFase != BetweenFase)
 			{
 				_anim->playAnim(AnimatedSpriteComponent::SpentaIdle);
 				_actualState = Moving;
 			}
-			_armVision = true;;
 
+			_armVision = true;;
 			_doSomething = random(900, 1300);
 			_timeOnMelee = 0;
 		}
@@ -195,14 +197,13 @@ void Boss1::checkMelee(const double& deltaTime)
 
 void Boss1::armShoot(const double& deltaTime)
 {
-	//_arm->shoot();
 	_bodyPos = Vector2D(_body->getBody()->GetPosition().x * M_TO_PIXEL, _body->getBody()->GetPosition().y * M_TO_PIXEL);
 	_dirB = (_bodyPos.getX() >= _playerPos.getX()) ? -1 : 1;
 	_timeOnShooting += deltaTime;
 	_armVision = false;
+
 	if (_actualBullet == 0 && !ida)
 	{
-
 		_timeOnShooting = 0;
 		_actualState = Moving;
 		_armVision = true;
@@ -210,7 +211,8 @@ void Boss1::armShoot(const double& deltaTime)
 		_timeBeetwenBullets = 50;
 		_doSomething = random(1200, 1600);
 	}
-	else {
+	else
+	{
 		if (_actualBullet == 0 && ida)
 		{
 			_anim->playAnim(AnimatedSpriteComponent::SpentaWing);
@@ -227,14 +229,10 @@ void Boss1::armShoot(const double& deltaTime)
 			_timeBeetwenBullets += 50;
 		}
 	}
-
-
-	//_myGun->enemyShoot(_myBulletPool, _bodyPos, !_anim->isFlipped() ? angle : angle + 180, "EnemyBullet");
 }
 
 void Boss1::orbAttack()
 {
-
 	if (_anim->animationFinished())
 	{
 		throwOrb();
@@ -245,40 +243,32 @@ void Boss1::orbAttack()
 			_actualNumOrbs = 0;
 			_actualState = Moving;
 			_anim->playAnim(AnimatedSpriteComponent::SpentaIdle);
-
 			_doSomething = random(1000, 2500);
 		}
 		else
 		{
-			//Provisional
 			_anim->playAnim(AnimatedSpriteComponent::SpentaIdle);
-			_anim->playAnim(AnimatedSpriteComponent::SpentaOrb);//Sera animacion de orbAttack
+			_anim->playAnim(AnimatedSpriteComponent::SpentaOrb);
 		}
-
 	}
-
 }
 
 void Boss1::beginCollision(GameObject * other, b2Contact * contact)
 {
 	Boss::beginCollision(other, contact);
 
-	string otherTag = other->getTag();
-
-	if (otherTag == "Misil" && isbeetweenFases())
+	if (other->getTag() == "Misil" && isbeetweenFases())
 	{
 		if (_lastFase == Fase1)
 			changeFase(Fase2);
 		else if (_lastFase == Fase2)
 			changeFase(Fase3);
 		else
-		{
 			die();
-		}
+
 		_anim->playAnim(AnimatedSpriteComponent::SpentaEndShield);
 		_bossPanel->updateLifeBar(_life1.getLife(), _life2.getLife(), _life3.getLife(), _life.getLife());
 	}
-
 }
 
 void Boss1::manageLife(Life& l, int damage)
@@ -303,19 +293,16 @@ void Boss1::fase1(const double& deltaTime)
 			if (_noAction > _doSomething)
 			{
 				int ra = random(0, 100);
-
 				if (ra >= 40)
 				{
 					_actualState = Meleeing;
 					_noAction = 0;
 					meleeAttack();
 				}
-
 				else
 				{
 					armShoot(deltaTime);
 					_actualState = Shooting;
-
 					_noAction = 0;
 				}
 			}
@@ -328,6 +315,7 @@ void Boss1::fase1(const double& deltaTime)
 	else
 		armShoot(deltaTime);
 }
+
 void Boss1::fase2(const double& deltaTime)
 {
 	if (_actualState != Bombing)
@@ -344,7 +332,6 @@ void Boss1::fase2(const double& deltaTime)
 						_actualState = Bombing;
 						bomberAttack(deltaTime, 100, 200);
 						_anim->playAnim(AnimatedSpriteComponent::SpentaStartBomb);
-
 						_noAction = 0;
 					}
 					else
@@ -365,6 +352,7 @@ void Boss1::fase2(const double& deltaTime)
 	else
 		bomberAttack(deltaTime, 100, 200);
 }
+
 void Boss1::fase3(const double& deltaTime)
 {
 	if (_actualState != Bombing)
@@ -412,7 +400,6 @@ void Boss1::beetwenFases(const double& deltaTime)
 	{
 		_anim->playAnim(AnimatedSpriteComponent::SpentaLoopShield);
 	}
-
 }
 
 void Boss1::changeFase(int nextFase)
@@ -422,7 +409,6 @@ void Boss1::changeFase(int nextFase)
 	_timeOnBomberAttack = 0;
 	_timeBeetwenBombs = 0;
 }
-
 
 void Boss1::throwBomb()
 {
@@ -443,6 +429,7 @@ void Boss1::shootBullet()
 		shoot();
 		_angle += _angleIncrease * _dirB;
 		_actualBullet++;
+
 		if (_actualBullet == 10)
 		{
 			ida = false;
@@ -453,15 +440,11 @@ void Boss1::shootBullet()
 	{
 		_angle -= _angleIncrease * _dirB;
 		shoot();
-
 		_actualBullet--;
 	}
 }
-
 
 void Boss1::shoot()
 {
 	_myGun->enemyShoot(_myBulletPool, _bodyPos, _angle, "EnemyBullet");
 }
-
-
